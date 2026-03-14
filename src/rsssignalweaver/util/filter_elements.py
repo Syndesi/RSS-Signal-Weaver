ad658e9d1290593357594bee0d53ad2ba6beabf2
Sören Klein <soerenklein98@gmail.com>
2026-03-14 13:10:57 +0100

    improve tests

--- src/rsssignalweaver/util/filter_elements.py
+++ src/rsssignalweaver/util/filter_elements.py
@@ -1,12 +1,9 @@
 from __future__ import annotations
 
 from collections.abc import Iterable
-from typing import TypeVar, Type
 
 from ..type import Element
 
-T = TypeVar('T', bound=Element)
 
-
-def filter_elements(items: Iterable[Element], cls: Type[T]) -> list[T]:
+def filter_elements[T: Element](items: Iterable[Element], cls: type[T]) -> list[T]:
 	return [e for e in items if isinstance(e, cls)]
